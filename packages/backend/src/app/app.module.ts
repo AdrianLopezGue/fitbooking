6e3ffb3d204edd90517c7400a9a760079fb1febb
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2023-06-13 18:06:17 +0200

    remove: useless scaffolding services

--- packages/backend/src/app/app.module.ts
+++ packages/backend/src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BootstrapModule } from './bootstrap.module';
 
 @Module({
   imports: [BootstrapModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
