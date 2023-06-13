5b7b60bb659e5ee4617424d2927a4ef1d7756801
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2023-06-13 18:04:12 +0200

    feat: add event to event transformer

---
+++ packages/backend/src/app/session/infrastructure/event-transformer/dtos/create-session.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsUUID } from 'class-validator';
+
+export class CreateSessionDTO {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsUUID()
+  readonly _id: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  readonly maxCapacity: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  readonly assistants: Array<string>;
+}
--- packages/backend/src/app/session/infrastructure/event-transformer/event-transformer.ts
+++ packages/backend/src/app/session/infrastructure/event-transformer/event-transformer.ts
@@ -1,8 +1,16 @@
 import { Event } from '@aulasoftwarelibre/nestjs-eventstore';
 import { SessionSeatWasBookedEvent } from '../../domain/event/session-seat-was-booked.event';
 import { BookSeatDTO } from './dtos/book-seat.dto';
+import { SessionWasCreatedEvent } from '../../domain/event/session-was-created.event';
+import { CreateSessionDTO } from './dtos/create-session.dto';
 
 export const eventTransformers = {
   SessionSeatWasBookedEvent: (event: Event<BookSeatDTO>) =>
     new SessionSeatWasBookedEvent(event.payload._id, event.payload.assistant),
+  SessionWasCreatedEvent: (event: Event<CreateSessionDTO>) =>
+    new SessionWasCreatedEvent(
+      event.payload._id,
+      event.payload.assistants,
+      event.payload.maxCapacity,
+    ),
 };
