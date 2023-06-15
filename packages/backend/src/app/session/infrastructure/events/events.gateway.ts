46bcac6a32f5a5b638a96dac3a088d7e84533c6b
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2023-06-15 14:59:47 +0200

    remove: unused functions in websocket gateway

--- packages/backend/src/app/session/infrastructure/events/events.gateway.ts
+++ packages/backend/src/app/session/infrastructure/events/events.gateway.ts
@@ -1,15 +1,10 @@
 import {
   ConnectedSocket,
-  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
-  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
-  WsException,
-  WsResponse,
 } from '@nestjs/websockets';
-import { from, Observable } from 'rxjs';
 import { Server, Socket } from 'socket.io';
 
 interface Reservation {
@@ -30,55 +25,9 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   handleConnection(@ConnectedSocket() client: Socket) {
     console.log(`Client connected: ${client.id}`, this.reservations);
-    this.server.emit('reservationsCount', this.reservations.size);
-    this.sendReservationStatus(client);
   }
 
   handleDisconnect(@ConnectedSocket() client: Socket) {
     console.log(`Client disconnected: ${client.id}`, this.reservations);
-    if (this.reservations.has(client.id)) {
-      this.releaseSession(undefined, client);
-    }
-    this.server.emit('reservationsCount', this.reservations.size);
-  }
-
-  @SubscribeMessage('reserveSession')
-  reserveSession(@MessageBody() _data: unknown, @ConnectedSocket() client: Socket): void {
-    if (!this.reservations.has(client.id)) {
-      const reservation: Reservation = {
-        clientId: client.id,
-        reservedBy: client.id,
-      };
-      this.reservations.set(client.id, reservation);
-      this.server.emit('classReserved');
-      this.server.emit('reservationsCount', this.reservations.size);
-      this.sendReservationStatus(client);
-      console.log(`Session reserved by client: ${client?.id}`);
-    }
-  }
-
-  @SubscribeMessage('releaseSession')
-  releaseSession(@MessageBody() _data: unknown, @ConnectedSocket() client: Socket): void {
-    if (this.reservations.has(client.id)) {
-      this.reservations.delete(client.id);
-      this.server.emit('classReleased');
-      this.server.emit('reservationsCount', this.reservations.size);
-      this.sendReservationStatus(client);
-      console.log(`Session released by client: ${client.id}`);
-    }
-
-    throw new WsException('Cannot reserve from another person.');
-  }
-
-  @SubscribeMessage('getReservations')
-  getReservations(): Observable<WsResponse<number>> {
-    const reservationsCount = this.reservations.size;
-    return from([{ event: 'reservationsCount', data: reservationsCount }]);
-  }
-
-  private sendReservationStatus(client: Socket): void {
-    const reservation = this.reservations.get(client.id);
-    const isReservedByClient = reservation && reservation.reservedBy === client.id;
-    client.emit('reservationStatus', isReservedByClient);
   }
 }
