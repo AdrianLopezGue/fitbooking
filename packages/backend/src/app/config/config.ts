790da8e4934636413fcb5a8e10ad224f283d2672
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2023-06-20 16:57:23 +0200

    feat(backend): add config variables to generate jwt and env test

--- packages/backend/src/app/config/config.ts
+++ packages/backend/src/app/config/config.ts
@@ -6,4 +6,8 @@ export default () => ({
     category: process.env.EVENTSTORE_STREAM,
     connection: process.env.EVENTSTORE_URL || 'esdb://eventstore:2113?tls=false',
   },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expires: process.env.JWT_EXPIRES || '60d',
+  },
 });
