de4a8f8a96ddde2428537c9671cf3706b420e2e6
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2023-06-20 16:02:49 +0200

    feat(backend): create test for invite athlete use case

---
+++ packages/backend/src/app/box/application/command/tests/invite-athlete-handler.spec.ts
@@ -0,0 +1,58 @@
+import { InMemoryBoxRepository } from '../../../infrastructure/repository/in-memory-box.repository';
+import { InviteAthleteHandler } from '../invite-athlete.handler';
+import { UserBuilder } from '../../../../test/user.builder';
+import { BoxBuilder } from '../../../../test/box.builder';
+import { InviteAthleteCommand } from '../invite-athlete.command';
+import { BoxId } from '../../../domain/model/box-id';
+import { Athlete } from '../../../domain/model/athlete';
+import { AthleteId } from '../../../domain/model/athlete-id';
+import { AthleteRole } from '../../../domain/model/athlete-role';
+
+describe('Invite athlete handler', () => {
+  it('should invite an athlete', async () => {
+    const user = new UserBuilder().build();
+    const box = new BoxBuilder().build();
+    const boxRepository = new InMemoryBoxRepository([box]);
+    const handler = new InviteAthleteHandler(boxRepository);
+
+    const result = await handler.execute(
+      new InviteAthleteCommand(box.id.value, user.email.value),
+    );
+
+    expect(result.isOk()).toBe(true);
+
+    const boxsFound = await boxRepository.findAll();
+    expect(boxsFound).toHaveLength(1);
+    expect(boxsFound[0].athletes).toHaveLength(1);
+    expect(boxsFound[0].athletes[0].email.value).toBe(user.email.value);
+  });
+
+  it('should return error if box is not found', async () => {
+    const user = new UserBuilder().build();
+    const boxRepository = new InMemoryBoxRepository([]);
+    const handler = new InviteAthleteHandler(boxRepository);
+
+    const result = await handler.execute(
+      new InviteAthleteCommand(BoxId.generate().value, user.email.value),
+    );
+
+    expect(result.isErr()).toBe(true);
+  });
+
+  it('should return error if email belongs to a current athlete in box', async () => {
+    const user = new UserBuilder().build();
+    const box = new BoxBuilder()
+      .withAthletes([
+        new Athlete(AthleteId.generate(), user.email, undefined, AthleteRole.basic()),
+      ])
+      .build();
+    const boxRepository = new InMemoryBoxRepository([box]);
+    const handler = new InviteAthleteHandler(boxRepository);
+
+    const result = await handler.execute(
+      new InviteAthleteCommand(BoxId.generate().value, user.email.value),
+    );
+
+    expect(result.isErr()).toBe(true);
+  });
+});
---
+++ packages/backend/src/app/test/box.builder.ts
@@ -0,0 +1,25 @@
+import { Athlete } from '../box/domain/model/athlete';
+import { Box } from '../box/domain/model/box';
+import { BoxId } from '../box/domain/model/box-id';
+import { BoxName } from '../box/domain/model/box-name';
+
+export class BoxBuilder {
+  public id: string;
+  public name: string;
+  public athletes: Athlete[];
+
+  public constructor() {
+    this.id = '0f6e8a59-7429-4930-9d38-3294552d2e40';
+    this.name = 'Defaul name';
+    this.athletes = [];
+  }
+
+  build(): Box {
+    return new Box(BoxId.from(this.id), BoxName.from(this.name), this.athletes);
+  }
+
+  withAthletes(athletes: Athlete[]): BoxBuilder {
+    this.athletes = athletes;
+    return this;
+  }
+}
