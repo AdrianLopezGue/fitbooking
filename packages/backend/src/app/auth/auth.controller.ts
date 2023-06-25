6c4113985416e80076b72b03ebb82fffb0daba87
Adrian Lopez <adrian.lopez.gue@hotmail.com>
2023-06-25 12:46:49 +0200

    fix(backend): add public to login endpoint

--- packages/backend/src/app/auth/auth.controller.ts
+++ packages/backend/src/app/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Post, UnauthorizedException, HttpCode } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
+import { Public } from './public.decorator';
 
 @Controller('login')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post()
+  @Public()
   @HttpCode(200)
   async login(
     @Body() loginDTO: { email: string; password: string },
